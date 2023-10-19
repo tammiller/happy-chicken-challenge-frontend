3d8e47122ec3319e58affa79c4ad3efe21446485
salwacynthia <salwadhaka@gmail.com>
2023-10-19 12:01:53 +0200

    progress sceen

---
+++ src/components/progressScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View } from 'react-native';
+import CircularProgress from 'react-native-circular-progress';
+import Icon from './Icon'; 
+
+
+const ProgressScreen = () => {
+   
+  const percentage = 70; // Change this with props
+  const currentDate = new Date();
+
+  const formattedDate = currentDate.toDateString();
+
+  return (
+
+    <View>
+        <View>
+            <Icon source = {require('../../assets/bellIcon.png')} width ={10} height={10}  />  
+            <Icon source = {require('../../assets/ringIcon.png')} width ={10} height={10}  />  
+            <Icon/>
+        </View>
+
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <CircularProgress
+        size={200} 
+        width={15} 
+        fill={percentage} 
+        tintColor="#00E0D7" 
+        backgroundColor="#ccc" 
+      >
+        {(fill) => (
+          <Text style={{ fontSize: 24 }}>{Math.round(fill)}%</Text>
+        )}
+      </CircularProgress>
+    </View>
+
+        <View> 
+         <Text> Today : {formattedDate}</Text>
+        </View>
+
+
+       <View>
+       
+        <Icon source = {require('../../assets/imoji.png')} width ={30} height={20}  /> 
+       </View>
+
+       <View>
+        <Icon source = {require('../../assets/home.png')} width ={15} height={15}  /> 
+        <Icon source = {require('../../assets/journey.png')} width ={15} height={15}  /> 
+        <Icon source = {require('../../assets/challenge.png')} width ={15} height={15}  /> 
+       </View>
+
+    </View>
+  );
+};
+
+export default ProgressScreen;
