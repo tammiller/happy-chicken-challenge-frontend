7a479e23dee25228c5eea10d285c2df10f5fc4e0
Amber Hunt <amberhunt955@gmail.com>
2023-10-20 21:04:33 -0400

    api call log in user

--- screens/CreateAccount.tsx
+++ screens/CreateAccount.tsx
@@ -1,100 +1,107 @@
 import { View, Text, StyleSheet, Pressable, TextInput, Alert } from "react-native";
 import { useState } from "react";
 
-export default function CreateAccount( {navigation}: { navigation: any } ) {
+export default function CreateAccount({ navigation }: { navigation: any }) {
+  const [email, setEmail] = useState("");
+  const [password, setPasswrod] = useState("");
+  const [name, setName] = useState("");
 
-  const[email, setEmail] = useState('');
-  const[password, setPasswrod] = useState('');
-  const[name, setName] = useState('');
+  const [isNameValid, setNameValid] = useState(false);
+  const [isEmailValid, setEmailValid] = useState(false);
+  const [isPsswordValid, setPsswordValid] = useState(false);
 
-  const[isNameValid, setNameValid] = useState(false);
-  const[isEmailValid, setEmailValid] = useState(false);
-  const[isPsswordValid, setPsswordValid] = useState(false);
-  
-
-   function onNameChangeHandler(enteredText: string){
-      setName(enteredText);
+  function onNameChangeHandler(enteredText: string) {
+    setName(enteredText);
     //   console.log('name '+name.length);
-      setNameValid(name !== '' ? name.length>0 : false);
-      return isNameValid;
-   }
+    setNameValid(name !== "" ? name.length > 0 : false);
+    return isNameValid;
+  }
 
-   function onEmailChangeHandler(enteredText: string){
+  function onEmailChangeHandler(enteredText: string) {
     setEmail(enteredText);
     let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-    setEmailValid(email.length>0 ? reg.test(enteredText) === true : false);
+    setEmailValid(email.length > 0 ? reg.test(enteredText) === true : false);
     return isEmailValid;
-   }
+  }
 
-   function onPasswordChangeHandler(enteredText: string){
+  function onPasswordChangeHandler(enteredText: string) {
     setPasswrod(enteredText);
-    setPsswordValid(password.length>0);
+    setPsswordValid(password.length > 0);
     return isPsswordValid;
-   }
+  }
 
-   const invalidAlert = ( fieldName: string) => {
-   // console.log("invalid Alert called for "+fieldName);
+  const invalidAlert = (fieldName: string) => {
+    // console.log("invalid Alert called for "+fieldName);
     Alert.alert(
-      'Invalid Entry!',
-      'Please enter a valid '+fieldName,
+      "Invalid Entry!",
+      "Please enter a valid " + fieldName,
       [
         {
-          text: 'OK',
+          text: "OK",
           onPress: () => {},
-          style: 'cancel',
+          style: "cancel",
         },
       ],
       {
         cancelable: true,
-       
-      },
+      }
     );
-   }
-
+  };
 
   return (
     <View style={mainContainerStyle.container}>
       <View style={containerStyles.container}>
         <Text style={textStyles.title}>Create your account</Text>
-        <View style={{width: '100%', height: 40}}></View>
+        <View style={{ width: "100%", height: 40 }}></View>
         <Text style={textStyles.body}>Name</Text>
-        <TextInput style={textInputStyles(isNameValid).textInput} onChangeText={onNameChangeHandler}></TextInput>
+        <TextInput
+          style={textInputStyles(isNameValid).textInput}
+          onChangeText={onNameChangeHandler}
+        ></TextInput>
         <Text style={textStyles.body}>Email</Text>
-        <TextInput style={textInputStyles(isEmailValid).textInput} onChangeText={onEmailChangeHandler}></TextInput>
+        <TextInput
+          style={textInputStyles(isEmailValid).textInput}
+          onChangeText={onEmailChangeHandler}
+        ></TextInput>
         <Text style={textStyles.body}>Password</Text>
-        <TextInput style={textInputStyles(isPsswordValid).textInput} onChangeText={onPasswordChangeHandler}></TextInput>
-   
+        <TextInput
+          style={textInputStyles(isPsswordValid).textInput}
+          onChangeText={onPasswordChangeHandler}
+        ></TextInput>
       </View>
       <View style={buttonContainerStyle.container}>
         <Pressable
           style={buttonStyles.button}
           onPress={() => {
             /* 1. Navigate to the GoalSetting route with params */
-           // console.log("isNameValid: "+isNameValid+ " with "+name +" isEmailValid: "+isEmailValid+ " isPasswordValid: "+isPsswordValid );
-            if(!isNameValid){invalidAlert('Name')}
-            else if(!isEmailValid){invalidAlert('Email id')}
-            else if(!isPsswordValid){invalidAlert('Password')}
-            else{
+            // console.log("isNameValid: "+isNameValid+ " with "+name +" isEmailValid: "+isEmailValid+ " isPasswordValid: "+isPsswordValid );
+            if (!isNameValid) {
+              invalidAlert("Name");
+            } else if (!isEmailValid) {
+              invalidAlert("Email id");
+            } else if (!isPsswordValid) {
+              invalidAlert("Password");
+            } else {
               navigation.navigate("Walkthrough1", {
                 itemId: 86,
                 otherParam: "anything you want here",
               });
-          }
+            }
           }}
         >
-        <Text style={buttonStyles.buttonText}>Create Account</Text>
+          <Text style={buttonStyles.buttonText}>Create Account</Text>
         </Pressable>
       </View>
       {/* <View style={buttonContainerStyle.linkContainer}> */}
-      <Pressable 
-      onPress={()=>{
+      <Pressable
+        onPress={() => {
           navigation.navigate("LogIn", {
             itemId: 86,
-            otherParam: "some"
-          })
-      }}
+            otherParam: "some",
+          });
+        }}
       >
-      <Text style={textStyles.linkText}>Already have account?Login</Text>
+        <Text style={textStyles.linkText}>Already have account?Login</Text>
       </Pressable>
     </View>
     // </View>
@@ -105,14 +112,12 @@ const buttonContainerStyle = StyleSheet.create({
   container: {
     width: "100%",
     alignItems: "center",
-   
   },
   linkContainer: {
     width: "100%",
-    justifyContent: 'center',
-    marginBottom: 50
-    
-  }
+    justifyContent: "center",
+    marginBottom: 50,
+  },
 });
 
 const mainContainerStyle = StyleSheet.create({
@@ -120,67 +125,63 @@ const mainContainerStyle = StyleSheet.create({
     width: "100%",
     height: "100%",
     justifyContent: "center",
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
 
 const containerStyles = StyleSheet.create({
   container: {
-    flex:1,
-    width: '100%',
+    flex: 1,
+    width: "100%",
     paddingTop: 30,
     paddingLeft: 30,
-    paddingRight: 30
-    
+    paddingRight: 30,
   },
 });
 
 const textStyles = StyleSheet.create({
-    title: {
-      fontSize: 24,
-      fontWeight: "bold",
-      textAlign: "left",
-      marginTop:50,
-  
-    },
-    subtitle: {
-      fontSize: 20,
-      textAlign: "left",
-      marginTop: 34,
-    },
-    body: {
-      fontSize: 18,
-      textAlign: "left",
-      marginTop: 20,
-    
-    },
-    textInput: {
-      width: '100%',
-      height: 40,
-      backgroundColor: '#F5F5F5',
-      marginTop: 10,
-      borderRadius: 4,
-    },
-    linkText: {
-      fontSize: 14,
-      textAlign: 'center',
-      marginBottom: 40
-    }
-  });
-
-  const textInputStyles = (isValid: boolean)=>StyleSheet.create({
-   textInput: { 
-    width: '100%',
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginTop: 50,
+  },
+  subtitle: {
+    fontSize: 20,
+    textAlign: "left",
+    marginTop: 34,
+  },
+  body: {
+    fontSize: 18,
+    textAlign: "left",
+    marginTop: 20,
+  },
+  textInput: {
+    width: "100%",
     height: 40,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "#F5F5F5",
     marginTop: 10,
     borderRadius: 4,
-    
-    padding: 5
+  },
+  linkText: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 40,
+  },
+});
 
-  }
+const textInputStyles = (isValid: boolean) =>
+  StyleSheet.create({
+    textInput: {
+      width: "100%",
+      height: 40,
+      backgroundColor: "#F5F5F5",
+      marginTop: 10,
+      borderRadius: 4,
 
-  })
+      padding: 5,
+    },
+  });
 
 const buttonStyles = StyleSheet.create({
   button: {
